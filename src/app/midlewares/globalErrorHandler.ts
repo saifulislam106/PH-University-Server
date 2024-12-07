58164f93e36db54cf847d017a346cde893a0c73c
saifulislam106 <saifulislam106915@gmail.com>
2024-12-08 00:09:26 +0600

    handleZodError commited

--- src/app/midlewares/globalErrorHandler.ts
+++ src/app/midlewares/globalErrorHandler.ts
@@ -1,18 +1,56 @@
+
+
+/* eslint-disable no-unused-expressions */
+
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+import { TErrorSource } from '../interface/error';
+import { ZodIssue } from 'zod-validation-error';
 
-const globalErrorHandler = (err:any , req:Request , res:Response ,next:NextFunction)=>{
-    const statusCode =500;
-    const message = err.message || "Something went wrong";
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong';
 
-    return res.status(statusCode).json({
-        success:false,
-        message,
-        error:err
+  let errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
 
+  const handleZodError = (err:ZodError)=>{
+
+    const errorSources :TErrorSource = err.issues.map((issue:ZodIssue)=>{
+        return{
+            path:issue?.path [issue.path.length-1],
+            message:issue.message
+        }
     })
-}
+    const statusCode = 400;
+
+    return{
+        statusCode,
+        message : "Validation Error",
+        errorSources
+    }
+  }
+
+  if (err instanceof ZodError) {
+    const simplifiedZodError = handleZodError(err)
+    message= simplifiedZodError.message,
+    statusCode =simplifiedZodError.statusCode,
+    errorSources = simplifiedZodError.errorSources
+  }
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    // error: err,
+  });
+};
 export default globalErrorHandler;
--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { StudentControllers } from './student.controller';
+import validateRequest from '../../midlewares/validateRequest';
+import { updateStudentValidationSchema } from './student.validation';
 
 const router = express.Router();
 
 router.get('/:studentId', StudentControllers.getSingleStudent);
 
-router.delete('/:studentId', StudentControllers.deleteStudent);
+router.delete('/:studentId',  StudentControllers.deleteStudent);
+router.patch('/:studentId',validateRequest(updateStudentValidationSchema), StudentControllers.updateStudent);
 
 router.get('/', StudentControllers.getAllStudents);
 
